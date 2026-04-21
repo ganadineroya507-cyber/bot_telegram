8a289711cd6630950fe0a32cc86908e4a4b4bc55
ganadineroya507-cyber <ganadineroya507@gmail.com>
2026-04-21 03:34:03 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -267,7 +267,7 @@ def pagar(id):
     return redirect("/")
 
 def run_web():
-    web.run(host="0.0.0.0", port=8080)
+    web.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 # ===== RUN =====
 app = ApplicationBuilder().token(TOKEN).build()
